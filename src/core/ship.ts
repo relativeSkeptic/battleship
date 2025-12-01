3d3e0e18a9a8de2c148686d89fb4a362cc1da93b
Dakota Stroud <str.dak97@gmail.com>
2025-11-30 19:13:23 -0600

    updating ship class to include shipType

--- src/core/ship.ts
+++ src/core/ship.ts
@@ -1,13 +1,20 @@
+import { ShipTypes } from "../types/shipTypes";
+
 export class Ship {
     private _length: number;
     private _numHits: number = 0;
     private _isSunk: boolean = false;
+    private _shipType: ShipTypes;
 
-    constructor(length: number) {
+    constructor(shipType: ShipTypes, length: number) {
         if (length < 1) {
             throw new Error("Length must be positive");
         }
+        if (shipType === ShipTypes.none) {
+            throw new Error("Invalid ship type");
+        }
         this._length = length;
+        this._shipType = shipType;
     }
 
     get length(): number {
@@ -20,6 +27,10 @@ export class Ship {
 
     get isSunk(): boolean {
         return this._isSunk;
+    }
+
+    get shipType(): ShipTypes {
+        return this._shipType;
     }
 
     hit(): void {
--- tests/ship.test.ts
+++ tests/ship.test.ts
@@ -1,27 +1,30 @@
 import { Ship } from "../src/core/ship";
+import { ShipTypes } from "../src/types/shipTypes";
 
 test("throws an error when a negative length is provided", () => {
-  expect(() => new Ship(-5)).toThrow("Length must be positive");
+    expect(() => new Ship(ShipTypes.battleship, -5)).toThrow(
+        "Length must be positive",
+    );
 });
 
 test("increments numHits when hit is called", () => {
-  let ship = new Ship(3);
-  ship.hit();
-  expect(ship.numHits).toBe(1);
+    let ship = new Ship(ShipTypes.destroyer, 3);
+    ship.hit();
+    expect(ship.numHits).toBe(1);
 });
 
 test("returns true for isSunk after all parts are hit", () => {
-  let ship = new Ship(3);
-  ship.hit();
-  ship.hit();
-  ship.hit();
-  expect(ship.isSunk).toBe(true);
+    let ship = new Ship(ShipTypes.submarine, 3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk).toBe(true);
 });
 
 test("after ship is sunk, hit function can no longer be called", () => {
-  let ship = new Ship(3);
-  ship.hit();
-  ship.hit();
-  ship.hit();
-  expect(() => ship.hit()).toThrow("Ship has already been sunk");
+    let ship = new Ship(ShipTypes.submarine, 3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(() => ship.hit()).toThrow("Ship has already been sunk");
 });
